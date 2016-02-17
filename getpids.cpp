8b2e026ff7121d123a6c6a53a8a9d1ef56998891
Abby Meunier <abby.meunier@gmail.com>
2016-02-17 21:46:25 +0000

    Prints some symlinks, many not found

--- getpids.cpp
+++ getpids.cpp
@@ -3,14 +3,19 @@
 #include <dirent.h>
 #include <string>
 #include <cstring>
-#include <cstdio>
+#include <unistd.h>
+#include <map>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	int pid;
-	int count = 0;
-	
+	string pid_string, path;
+	const char *c_path;
+	char buf[512];
+	int count; 
+	//map<int, string> pid_execs;
+
 	//open /proc/ directory
 	struct dirent *current;
 	DIR *pdir = opendir("/proc");
@@ -21,12 +26,23 @@ int main(int argc, char *argv[]) {
 	while((current = readdir(pdir))) {
 		//print numerical directory contents
 		pid = atoi(current->d_name);
-		if(pid > 0) {
-			cout << pid << endl;
-			count++;
+		if(pid > 0) { 
+			pid_string = current->d_name;
+			path = "/proc/" + pid_string + "/exe";
+			const char *c_path = path.c_str();
+			
+			count = readlink(c_path, buf, sizeof(buf) - 1);
+			
+			//print pid with corresponding exe symlink location
+			if(count >= 0) { 
+				buf[count] = '\0';		
+				cout << pid << ": " << buf << endl;
+			}
+			else 
+				cout << pid << ": no symlink found\n";
+		
 		}
 	}
-	cout << "Total pids: " << count << endl;
 
 	closedir(pdir);
 
